02a26482924010c76e1fc965c11fc372a0e1d133
mikhail.barkov <mikhail.barkov@uno-soft.ru>
2024-12-01 15:46:34 +0400

    Add cl count test

--- test/test_methylene_group_count.py
+++ test/test_methylene_group_count.py
@@ -12,6 +12,7 @@ def test_one_methylene_group_count():
     molecule = read_smiles('Oc1cccc(Cc2ccccc2)c1')
     assert methylene_group_count(molecule) == 1
 
+
 def test_two_methylene_group_count():
     molecule = read_smiles('Oc1cccc(CCc2cc(O)cc(O)c2)c1')
     assert methylene_group_count(molecule) == 2
