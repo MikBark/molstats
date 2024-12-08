679659302ff3febaceb2fd77d3e29d08fe51e3ee
mikhail.barkov <mikhail.barkov@uno-soft.ru>
2024-12-08 16:39:42 +0400

    Add string molecule passing

--- test/test_cl_count.py
+++ test/test_cl_count.py
@@ -1,8 +1,9 @@
 from pysmiles import read_smiles
 
-from smilestats import cl_count
+from smilestats import cl_count, graph_cl_count
 
 
 def test_cl_count():
-    molecule = read_smiles('Cc1cc(-c2cccc(Cl)c2)ccc1O')
+    molecule = 'Cc1cc(-c2cccc(Cl)c2)ccc1O'
     assert cl_count(molecule) == 1
+    assert graph_cl_count(read_smiles(molecule)) == 1
--- test/test_hydroxy_group_count.py
+++ test/test_hydroxy_group_count.py
@@ -1,13 +1,15 @@
 from pysmiles import read_smiles
 
-from smilestats import hydroxy_group_count
+from smilestats import graph_hydroxy_group_count, hydroxy_group_count
 
 
 def test_one_hydroxy_group():
-    molecule = read_smiles('Cc1cc(-c2cccc(Cl)c2)ccc1O')
+    molecule = 'Cc1cc(-c2cccc(Cl)c2)ccc1O'
     assert hydroxy_group_count(molecule) == 1
+    assert graph_hydroxy_group_count(read_smiles(molecule)) == 1
 
 
 def test_two_hydroxy_group():
-    molecule = read_smiles('Cc1cc(O)ccc1O')
+    molecule = 'Cc1cc(O)ccc1O'
     assert hydroxy_group_count(molecule) == 2
+    assert graph_hydroxy_group_count(read_smiles(molecule)) == 2
